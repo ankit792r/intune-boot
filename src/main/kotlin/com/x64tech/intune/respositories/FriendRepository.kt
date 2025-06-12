155da7d36bfff7ea5d1815e7e400b34229bc8d45
Ankit Prajapati <ap8424856@gmail.com>
2025-06-13 00:55:12 +0530

    updated friend repo query

--- src/main/kotlin/com/x64tech/intune/respositories/FriendRepository.kt
+++ src/main/kotlin/com/x64tech/intune/respositories/FriendRepository.kt
@@ -1,18 +1,17 @@
 package com.x64tech.intune.respositories
 
 import com.x64tech.intune.entites.FriendEntity
-import com.x64tech.intune.models.FriendView
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface FriendRepository: JpaRepository<FriendEntity, UUID> {
-    @Query("select f from FriendEntity f where f.initiator = :userId or f.acceptor = :userId")
+    @Query("select f from FriendEntity f where f.status not like 'REQUEST' and (f.initiator.id = :userId or f.acceptor.id = :userId) ")
     fun getUsersFriends(userId: UUID): List<FriendEntity>
 
-    @Query("select f from FriendEntity f where f.initiator = :userId and f.acceptor = :friendId")
+    @Query("select f from FriendEntity f where f.initiator.id = :userId and f.acceptor.id = :friendId")
     fun getFriendRequestSentAlready(userId: UUID, friendId: UUID): Optional<FriendEntity>
 
-    @Query("select f from FriendEntity f where f.initiator = :userId or f.acceptor = :userId and f.status in ('SENT', 'RECEIVE')")
+    @Query("select f from FriendEntity f where f.initiator.id = :userId or f.acceptor.id = :userId and f.status = 'REQUEST' ")
     fun getFriendRequests(userId: UUID): List<FriendEntity>
 }
